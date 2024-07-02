02b22ae187f30187b6b7a8170fecca30bff92127
Tamir <hero2000.ts@gmail.com>
2024-07-02 04:50:28 -0700

    teemo

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,17 @@
 #Installing flask using pip
 
 package { 'python3.8':
-  ensure   => '3.8.10',
-  provider => 'pip3'
+  ensure    => '3.8.10',
+  provider  => 'pip3'
 }
 
 package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require => Package['python3.8']
+  ensure    => '2.1.0',
+  provider  => 'pip3',
 }
 
 package { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-  require  => Package['Flask']
+  ensure    => '2.1.1',
+  provider  => 'pip3',
+  require   => Package['Flask']
 }
