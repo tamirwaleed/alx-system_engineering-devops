5fecac17a7908f5810a414dc9fdbd7f0d95bde68
Tamir <hero2000.ts@gmail.com>
2024-07-02 05:31:27 -0700

    teemo

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,7 @@ package { 'python3.8':
   provider => 'pip3',
 }
 
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
@@ -13,5 +13,5 @@ package { 'Flask':
 package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
-  require  => Package['Flask'],
+  require  => Package['flask'],
 }
