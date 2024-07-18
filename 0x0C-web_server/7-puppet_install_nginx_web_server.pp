8f5d9a7f0f01396531e3c6b2c56887071d2fc5cb
Tamir <hero2000.ts@gmail.com>
2024-07-18 12:34:23 -0700

    Another ALX Task

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# configuring a web server by puppet
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.linkedin.com/in/tamir-el-waleed/ permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
