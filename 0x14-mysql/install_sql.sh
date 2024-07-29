350d319f9433e96943f547fddceecf395eb922cf
Tamir <hero2000.ts@gmail.com>
2024-07-29 08:43:56 -0700

    Another ALX Task

--- 0x14-mysql/install_sql.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo wget -O mysql57 https://raw.githubusercontent.com/nuuxcode/alx-system_engineering-devops/master/scripts/mysql57 && sudo chmod +x mysql57 &&  sudo ./mysql57
-
