1e8fbc8fcae59e0a3376d2b0d05a9be606188c19
Tamir <hero2000.ts@gmail.com>
2024-07-31 04:49:48 -0700

    Another ALX Task

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
         todo_param = {"userId": use.get("id")}
         todo = requests.get(url + "todos", params=todo_param).json()
         theFile[use.get("id")] = [{"user": use.get("username"),
-                         "task": x.get("title"),
-                         "completed": x.get("completed")}
-                         for x in todo]
+                                   "task": x.get("title"),
+                                   "completed": x.get("completed")}
+                                  for x in todo]
     filename = "todo_all_employees.json"
     with open(filename, "w", newline='') as fd:
         json.dump(theFile, fd)
