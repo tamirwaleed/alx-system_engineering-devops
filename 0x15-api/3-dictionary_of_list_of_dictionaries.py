4b0331853afe50c78c5a8ddf8d2783819a9e0c1d
Tamir <hero2000.ts@gmail.com>
2024-07-31 04:48:05 -0700

    Another ALX Task

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' Export to JSON '''
+
+import csv
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    user = requests.get(url + "users").json()
+    theFile = {}
+    for use in user:
+        todo_param = {"userId": use.get("id")}
+        todo = requests.get(url + "todos", params=todo_param).json()
+        theFile[use.get("id")] = [{"user": use.get("username"),
+                         "task": x.get("title"),
+                         "completed": x.get("completed")}
+                         for x in todo]
+    filename = "todo_all_employees.json"
+    with open(filename, "w", newline='') as fd:
+        json.dump(theFile, fd)
