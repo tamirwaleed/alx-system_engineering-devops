0094d4e92aa67229e7ddeeb2dbaeb1eec59cd333
Tamir <hero2000.ts@gmail.com>
2024-07-31 02:43:13 -0700

    Another ALX Task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' Gather Data from an API '''
+
+import sys
+import requests
+import json
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    emp_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(emp_id)).json()
+    todo_param = {"userId": emp_id}
+    todo = requests.get(url + "todos", params=todo_param).json()
+    completed = []
+    for x in todo:
+        if x.get("completed") is True:
+            completed.append(x.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+                                                          len(completed),
+                                                          len(todo)))
+    for y in completed:
+        print("\t {}".format(y))
