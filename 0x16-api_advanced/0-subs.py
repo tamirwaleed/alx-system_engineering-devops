03e5375cb502406b48b39186bcdfaa9fc0736970
Tamir <hero2000.ts@gmail.com>
2024-08-13 08:21:31 -0700

    Another ALX Task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 ''' function that queries the Reddit API
 and returns the number of subscribers '''
-
-
 import json
 import requests
 import sys
 
 
 def number_of_subscribers(subreddit):
-    ''' the function '''
-    url = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
+    ''' Args:
+        subreddit name
+    Returns:
+        no of subs or 0 if failed '''
+    url = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
     headers = {'User-Agent': 'MyRedditApp/0.0.1'}
     redd = requests.get(url, headers=headers, allow_redirects=False)
     if redd.status_code == 200:
         results = redd.json()
-        return (results.get("data").get("subscribers"))
+        return results.get("data").get("subscribers")
     else:
-        return (0)
+        return 0
