71a5cf763d1ee6b4f9d1656e67aa46de008d220d
Tamir <hero2000.ts@gmail.com>
2024-08-13 08:09:15 -0700

    Another ALX Task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' function that queries the Reddit API
+and returns the number of subscribers '''
+
+
+import sys
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    ''' the function '''
+    url = 'https://reddit.com/r/{}/about/.json'.format(subreddit)
+    headers = {'User-Agent': 'MyRedditApp/0.0.1'}
+    redd = requests.get(url, headers=headers, allow_redirects=False)
+    if redd.status_code == 200:
+        results = redd.json()
+        return results.get("data").get("subscribers")
+    else:
+        return 0
