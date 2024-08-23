363100527c24680cef99284bba3eff761a72995f
Tamir <hero2000.ts@gmail.com>
2024-08-23 08:29:45 -0700

    Another ALX Task

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # changing the user limits
 
 exec {'change hard user limits':
-  command => 'sed -i "/holberton hard/s/4/50000/"  /etc/security/limits.conf',
+  command => 'sed -i "s/^holberton.*hard.*nofile.*/holberton hard nofile 50000/"  /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/',
 }
 
 exec {'change soft user limits':
-  command => 'sed -i "/holberton soft/s/5/50000/"  /etc/security/limits.conf',
+  command => 'sed -i "s/^holberton.*soft.*nofile.*/holberton soft nofile 50000/"  /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/',
 }
