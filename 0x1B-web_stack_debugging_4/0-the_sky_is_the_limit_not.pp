b1a0845b5ca4b848473f2ff93983dd86a0021408
Tamir <hero2000.ts@gmail.com>
2024-08-23 07:26:36 -0700

    Another ALX Task

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # reducing failures to 0
 
 exec {'sky is the limit':
-  command => 'sed -i "s/4096/15/" /etc/default/nginx && sudo service nginx restart'
+  command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart'
   path    => '/usr/bin:/bin/',
 }
