999c8a211ec04269aac61c471d7e3a01ea82a801
Tamir <hero2000.ts@gmail.com>
2024-09-27 03:33:30 -0700

    Another ALX Task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,8 +12,7 @@ def number_of_subscribers(subreddit):
         no of subs or 0 if failed '''
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-            "User-Agent":
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+            "User-Agent": 'TamirRedditApp/1.0 (by /u/coldshawerma)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         results = response.json()
